5f305fbddbdadcc690dadb15d2a0a21b1c661a88
Holger Adams <87605596+dm5tt@users.noreply.github.com>
2025-02-10 17:11:37 +0100

    lorawan_sensors: removed keys - that broke the build

--- apps/lorawan_sensors/src/lorawan_impl.h
+++ apps/lorawan_sensors/src/lorawan_impl.h
@@ -5,9 +5,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/lorawan/lorawan.h>
 
-#define LORAWAN_IMPL_DEV_EUI			{ }  /* MSB! Dummy Key - doesn't work! */
-#define LORAWAN_IMPL_JOIN_EUI	  		{ }  /* MSB!  Dummy Key - doesn't work! */
-#define LORAWAN_IMPL_APP_KEY			{ } /* MSB!  Dummy Key - doesn't work! */
+#define LORAWAN_IMPL_DEV_EUI			{ 0x00 }  /* MSB! Dummy Key - doesn't work! */
+#define LORAWAN_IMPL_JOIN_EUI	  	{ 0x00 }  /* MSB!  Dummy Key - doesn't work! */
+#define LORAWAN_IMPL_APP_KEY			{ 0x00 } /* MSB!  Dummy Key - doesn't work! */
 
 int lorawan_impl_init(const struct device *lora_dev);
 
